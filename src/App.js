fb49d315051147cd5ef4d81cc8ccb2e8e888c046
Anna Presnyakova <apresnyakova88@gmail.com>
2019-02-02 21:34:40 +0100

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid } from '@material-ui/core';
 
-class App extends Component {
-  render() {
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = theme => ({
+    main: {
+        width: 'auto',
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
+            width: 1100,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        }
+    }
+});
+
+function App(props) {
+    const { classes } = props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <main className={classes.main}>
+            <Grid container>
+                <Grid item xs={9}>
+                  list
+                </Grid>
+                <Grid item xs={3}>
+                    info
+                </Grid>
+            </Grid>
+        </main>
     );
-  }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
+

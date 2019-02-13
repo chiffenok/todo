02ec99014cbdf8ad8ad09ed1e302b88cddf45482
Anna Presnyakova <apresnyakova88@gmail.com>
2019-02-13 16:38:35 +0100

    handling error, reemplementing render for TodoList; emplemnting getTodosUser

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import {createMuiTheme, withTheme} from '@material-ui/core/styles';
+import { withTheme } from '@material-ui/core/styles';
 import {
     Grid,
     AppBar,
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,20 @@
 import React, {Component} from 'react';
-import {Checkbox, List, ListItem, ListItemText, CircularProgress} from "@material-ui/core";
+import {Checkbox, List, ListItem, ListItemText, CircularProgress, Typography} from "@material-ui/core";
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = theme => ({
+    listItem: {
+        backgroundColor: theme.palette.background.paper,
+        borderRadius: theme.shape.borderRadius * 2,
+        marginTop: theme.spacing.unit * 1
+    },
+    circularProgress : {
+        display: 'block',
+        marginTop: theme.spacing.unit * 3,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    }
+});
 
 class TodoList extends Component {
     constructor(props) {
@@ -7,16 +22,39 @@ class TodoList extends Component {
         this.state = {
             checked: [0],
             todosItems: null,
-            isLoading: false
+            isLoading: false,
+            error: null
         };
         this.renderList = this.renderList.bind(this);
     }
 
     componentDidMount() {
-        this.setState({isLoading: true})
+        this.setState({isLoading: true});
         fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(response => response.json())
-            .then(data => this.setState({todosItems: data, isLoading: false}));
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error(response.status + '! File is not found');
+                }
+            })
+            .then(data => this.setState({todosItems: data, isLoading: false}))
+            .catch(error => {
+                this.setState({error, isLoading: false});
+            } );
+    }
+
+    getTodosByUser() {
+        return this.state.todosItems.reduce((users, item) => {
+            const { userId } = item ;
+
+            users[userId] = users[userId]
+                ? [...users[userId], item]
+                : [item];
+
+            return users
+
+        }, {})
     }
 
 
@@ -36,11 +74,11 @@ class TodoList extends Component {
         });
     };
 
-    renderList = (items) => {
+    renderList = (items, classes) => {
         return(
             <List>
                 {items.slice(0, 20).map(value => (
-                    <ListItem key={value.id} role={undefined} dense button
+                    <ListItem className={classes.listItem} key={value.id} role={undefined} dense button
                               onClick={this.handleToggle(value.id)}>
                         <Checkbox
                             checked={this.state.checked.indexOf(value.id) !== -1}
@@ -55,15 +93,19 @@ class TodoList extends Component {
     };
 
     render () {
-        console.log(this.state.todosItems);
-        return (
-            (this.state.todosItems === null || this.state.isLoading) ? (
-                <CircularProgress/>
-            ) : (
-                this.renderList(this.state.todosItems)
-            )
-        );
+        const { classes } = this.props;
+        const { todosItems, isLoading, error} = this.state;
+
+        if (error) {
+            return <Typography component="p">{error.message}</Typography>
+        }
+
+        if (isLoading || todosItems === null) {
+            return <Typography component="p">Loading....</Typography>
+        }
+
+        return this.renderList(todosItems, classes);
     }
 }
 
-export default TodoList;
+export default withStyles(styles)(TodoList);
